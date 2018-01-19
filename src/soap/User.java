85df71b52b1306536fc5f77aab68b044038d724d
Unknown <julia.hermann93@gmail.com>
2018-01-19 17:26:40 +0100

    Adding content

--- src/soap/User.java
+++ src/soap/User.java
@@ -1,5 +1,8 @@
 package soap;
+import java.util.List;
+
 import javax.jws.WebMethod;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style; 
@@ -7,6 +10,17 @@ import javax.jws.soap.SOAPBinding.Use;
 //Service Endpoint Interface
 @WebService
 @SOAPBinding(style = Style.DOCUMENT, use=Use.LITERAL)
-public interface User{ 
-    @WebMethod String getUserNameAsString(String name);
+public interface User {
+
+	
+	@WebMethod(operationName="listUsers")
+	@WebResult(name="userList") 
+	public List<String> listUsers();
+
+
+	@WebMethod(operationName="checkUser")
+	@WebResult(name="userCheckResult") 
+	public String checkUser(String firstname,String lastname,String email, String birthyear);
+
+
 }
--- src/soap/UserImpl.java
+++ src/soap/UserImpl.java
@@ -1,11 +1,27 @@
 package soap;
 
+import java.util.List;
+
 import javax.jws.WebService;
+
+import business.CheckUser;
 //Service Implementation
 @WebService(endpointInterface = "soap.User")
 public class UserImpl implements User {
-  @Override
-  public String getUserNameAsString(String name) {
-      return "Username: " + name;
-  }
+
+	@Override
+	public List<String> listUsers() {
+		List<String> users = CheckUser.listUsers();		
+		return users;
+	}
+
+	@Override
+	public String checkUser(String firstname, String lastname, String email, String birthyear) {
+		String result = CheckUser.checkUser(firstname, lastname, email, birthyear);
+		return result;
+	}
+	
+	
+	
+
 }
